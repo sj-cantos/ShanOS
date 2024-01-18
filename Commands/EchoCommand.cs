ce8f2c2086dd46797f8473d5b0e276e7dac9b819
Shannon <shanjohncantz@gmail.com>
2024-01-18 21:02:52 +0900

    added echo command

---
+++ Commands/EchoCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShanOS.Commands
+{
+    internal class EchoCommand : Command
+    {
+        public EchoCommand() : base("echo") { }
+
+        public override string execute(string[] args)
+        {
+            // Join the arguments into a single string
+            string echoedText = string.Join(" ", args);
+
+            return echoedText;
+        }
+    }
+}
+
