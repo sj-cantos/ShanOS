08290302b95a918b98b2d204b29072a7aab09acf
Shannon <shanjohncantz@gmail.com>
2024-01-18 23:51:34 +0900

    added args implementation for creation and deletion of files

--- Commands/FileCommand.cs
+++ Commands/FileCommand.cs
@@ -1,18 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-
+using Sys = Cosmos.System;
 namespace ShanOS.Commands
 {
     internal class FileCommand : Command
     {
-        public FileCommand() : base("") { }
+        public FileCommand(string name) : base(name) { }
 
         public override string execute(string[] args)
         {
-            return base.execute(args);
+            string response = "";
+            switch (args[0])
+            {
+                
+                case "create":
+                    try
+                    {
+                        Sys.FileSystem.VFS.VFSManager.CreateFile(args[1]);
+                        response = $"Your file {args[1]} was created successfully.";
+                    }
+                    catch (Exception e)
+                    {
+                        response = $"File creation failed. Error: {e.ToString()}";
+                        break;
+                    }
+                    break;
+
+                case "delete":
+                    try
+                    {
+                        Sys.FileSystem.VFS.VFSManager.DeleteFile(args[1]);
+                        response = $"Your file {args[1]} was deleted successfully.";
+                    }
+                    catch (Exception e)
+                    {
+                        response = $"File deletion failed. Error: {e.ToString()}";
+                        break;
+                    }
+
+                    break;
+            }
+
+            return response;
         }
     }
 }
