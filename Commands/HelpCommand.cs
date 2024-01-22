f8c1038959a64ea4521ddd728f3217fdbb35455d
Shannon <shanjohncantz@gmail.com>
2024-01-23 00:01:09 +0900

    final natalaga pramis

--- Commands/HelpCommand.cs
+++ Commands/HelpCommand.cs
@@ -12,7 +12,7 @@ namespace ShanOS.Commands
 
         public override string execute(string[] args)
         {
-           
+
             return "------DISPLAYING HELP INFORMATION-------\n\n" +
                 "------GENERAL COMMANDS-------\n" +
                 "help\t - displays ommands \n" +
@@ -24,11 +24,13 @@ namespace ShanOS.Commands
                 "file mkdir [dir name]\t - creates a new directory\n"
                 + "file rmdir [dir name]\t - deletes a directory\n" +
                 "file write [filename.txt] [content]\t - writes content to a file\n" +
-                "file cat [filename.txt]\t - reads the content of a file\n" +
+                "file cat [filename.txt]\t - reads the content of a file\n\n" +
                 "-------MEMORY COMMANDS---------\n" +
                 "memory free - displays the available ram\n" +
-                "memory used - displays the used memory\n" + 
-                "memory allocated - displays the number of allocated objects\n";
+                "memory used - displays the used memory\n" +
+                "memory allocated - displays the number of allocated objects\n\n" +
+                "-------PROCESS COMMANDS---------\n" +
+                "ps - lists all the processes\n";
         }
     }
 }
